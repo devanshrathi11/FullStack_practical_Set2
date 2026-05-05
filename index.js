de8c5dc57fb7a7c02e1b02737fc93dfc7a7f3770
Devansh Rathi <142798393+devanshrathi11@users.noreply.github.com>
2026-05-05 12:41:07 +0530

    Update index.js

--- index.js
+++ index.js
@@ -4,40 +4,30 @@ const app = express();
 
 app.get('/', async (req, res) => {  
         const events = await Events.find().sort({ createdAt: -1 });
-        res.render('events/index', { events });
-        
+        res.render('events/index', { events });       
 });
 app.get('/new', (req, res) => {
     res.render('events/new', { events: new Events() });
 });
-
 app.post('/', async (req, res) => {
     const events = new Events(req.body); 
         await events.save();
-        
-
 });
 app.get('/:id', async (req, res) => { 
         const events = await Events.findById(req.params.id);    
-        res.render('events/show', { events });        
-    
+        res.render('events/show', { events });           
 });
 app.get('/:id/edit', async (req, res) => {
         const events = await Events.findById(req.params.id);
-        res.render('events/edit', { events });
-       
+        res.render('events/edit', { events });      
 });
-app.put('/:id', async (req, res) => {
-   
+app.put('/:id', async (req, res) => { 
         const events = await Events.findById(req.params.id);
-        delete req.body.title;
-    
-        await events.save();
-        
+        delete req.body.title;    
+        await events.save();       
 });
 app.delete('/:id', async (req, res) => {
         const events = await Events.findByIdAndDelete(req.params.id);     
-        res.redirect('/events');
-    
+        res.redirect('/events');    
 });
 module.exports = app;
