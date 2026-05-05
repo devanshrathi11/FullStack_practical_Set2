6688a961aafa1b69869b720060f0061781b6b2ea
Devansh Rathi <142798393+devanshrathi11@users.noreply.github.com>
2026-05-05 12:30:28 +0530

    Fix express app initialization and error handling

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 const express = require('express');
-const app = express;
-const Events = require('../models/Events');
+const app = express();
+
 
 app.get('/', async (req, res) => {  
         const events = await Events.find().sort({ createdAt: -1 });
         res.render('events/index', { events });
-        res.status(500).render('error',);   
+        
 });
 app.get('/new', (req, res) => {
     res.render('events/new', { events: new Events() });
@@ -14,8 +14,7 @@ app.get('/new', (req, res) => {
 app.post('/', async (req, res) => {
     const events = new Events(req.body); 
         await events.save();
-        res.redirect(`/events/${events._id}`);
-        res.status(400).render('events/new');
+        
 
 });
 app.get('/:id', async (req, res) => { 
@@ -26,7 +25,7 @@ app.get('/:id', async (req, res) => {
 app.get('/:id/edit', async (req, res) => {
         const events = await Events.findById(req.params.id);
         res.render('events/edit', { events });
-        res.status(500).render('error',);
+       
 });
 app.put('/:id', async (req, res) => {
    
@@ -34,7 +33,7 @@ app.put('/:id', async (req, res) => {
         delete req.body.title;
     
         await events.save();
-        res.redirect(`/events/${events._id}`);
+        
 });
 app.delete('/:id', async (req, res) => {
         const events = await Events.findByIdAndDelete(req.params.id);     
